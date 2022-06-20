985797c63fd0bffc13ef9b87c0b6d25be79efb4a
Gagan Gurung <gagan.gurung100@gmail.com>
2022-06-20 18:51:10 +0545

    feat: google verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="IlXTcu3lNZF7wI4KwkEvhH6f_3dTLOMePmV7Z1-0RmQ" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
